9c61efbf9d707918d448a3d840f06f33e58bf649
Hilke Ros <hilke@mmmotion.com>
2012-02-14 12:57:52 +0100

    eerste homepage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Creatiefschrijven::Application.config.secret_token = 'e6d35bd025971baa632a2edfa79449f9660d8f2f8cae18c68eef3fa406d372c92d60874664979bfb535d3d3e2815a7d3d95c25425ac9dcf1fb3378edbc734d8e'
